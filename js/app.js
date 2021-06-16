7a27b5e6b299c848d4da6323bd901bbcfdd0b6aa
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-06-16 00:45:14 -0300

    Main page preview

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@
 const $btnContinuar = document.querySelector("#btnContinuar");
 const $btnEmpezar = document.querySelector("#btnEmpezar");
 
+const body = document.querySelector("#body");
 const p01 = document.querySelector("#p01");
 const p02 = document.querySelector("#p02");
 const p03 = document.querySelector("#p03");
 
 setTimeout(function () {
 	p01.classList.add("avanzar");
-}, 1000);
+}, 2000);
 
 $btnContinuar.addEventListener("click", () => {
 	p02.classList.add("avanzar");
@@ -17,6 +18,7 @@ $btnContinuar.addEventListener("click", () => {
 
 $btnEmpezar.addEventListener("click", () => {
 	p03.classList.add("avanzar");
+	body.classList.remove("overflow-on");
 });
 
 //--------------------------------------
